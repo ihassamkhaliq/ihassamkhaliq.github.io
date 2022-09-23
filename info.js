d473b800022d12cc85435990d65758df0db9fc58
Hassam <asfandtnt@hotmail.com>
2022-09-23 06:35:54 -0700

    clear the extras

--- info.js
+++ info.js
@@ -290,26 +290,6 @@
         }
    
 
-    // Get the Subscription of User
-
-
-
-    //$$(Helper function for treating errors, $loc_script_taskpane_home_js_comment34$)$$
-    function errorHandler(error) {
-        // $$(Always be sure to catch any accumulated errors that bubble up from the Word.run execution., $loc_script_taskpane_home_js_comment35$)$$
-        showNotification("Error:", error);
-        console.log("Error: " + error);
-        if (error instanceof OfficeExtension.Error) {
-            console.log("Debug info: " + JSON.stringify(error.debugInfo));
-        }
-    }
-
-    function logout() {
-        console.log("I'm in the Logout Function")
-        localStorage.setItem("JWT", null)
-        location.assign('/Home.html')
-    }
-
     // Helper function for displaying notifications
     function showNotification(header, content) {
         $("#notification-header").text(header);
